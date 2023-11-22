cb785ba417f7e06d17fe320581a3f87072c5a401
Sakthilakshmi-M <sakthilakshmims@gmail.com>
2023-11-22 15:23:40 +0530

    render deploy

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,26 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require('cors');
 const auth = require("./routes/authRoute");
 const userRoute = require("./routes/userRoutes");
 const {mongoose} = require("mongoose");
+const cors = require("cors");
 const app = express()
-
-app.use(cors(
-  {
-    origin:["https://car-management-system-frontend.vercel.app/"],
-    methods: ["POST","GET"],
-    credentials: true
-  }
-))
 app.use(express.json())
 
+const corsOptions = {
+  origin: "http://localhost:3000" // frontend URI (ReactJS)
+}
+app.use(cors(corsOptions));
+
 app.use("/api/auth",auth);
-app.use("/api/user",userRoute);
+app.use("/api/user",userRoute)
 
 mongoose.connect(process.env.MONGO_URI)
-.then(
+.then(()=>{
+  const port = process.env.PORT||8000;
   app.listen(process.env.PORT,()=>{
     console.log("db is connected & Server is listening at port ",process.env.PORT);
-  })
+})}
 )
 .catch((err)=>{
   console.log(err);
