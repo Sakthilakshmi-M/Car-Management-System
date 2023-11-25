94f2ffe39574bb5310262e19071b1bfd457b428d
Sakthilakshmi-M <sakthilakshmims@gmail.com>
2023-11-25 20:57:55 +0530

    path changed

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use("/api/user",userRoute)
 
 app.use(express.static(path.join(__dirname,'./frontend/build')))
 app.get("*",function(req,res){
-  res.sendFile(path.join(__dirname,"./frontend/build/index.html"))
+  res.sendFile(path.join(__dirname,"./frontend/public/build/index.html"))
 })
 
 mongoose.connect(process.env.MONGO_URI)
