20facbe756492e064c375f4017d228924835ab62
Sakthilakshmi-M <sakthilakshmims@gmail.com>
2023-11-25 20:02:12 +0530

    folder structure changed

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use("/api/user",userRoute)
 
 //serving the frontend
 
-app.use(express.static(path.join(__dirname,'../frontend/build')))
+app.use(express.static(path.join(__dirname,'./frontend/build')))
 app.get("*",function(req,res){
-  res.sendFile(path.join(__dirname,"../frontend/build/index.html"))
+  res.sendFile(path.join(__dirname,"./frontend/build/index.html"))
 })
 
 mongoose.connect(process.env.MONGO_URI)
