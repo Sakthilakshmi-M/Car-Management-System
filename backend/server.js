83d18bbc3ddaa419b79c8f9022a9cd01e93593dd
Sakthilakshmi-M <sakthilakshmims@gmail.com>
2023-11-26 09:30:54 +0530

    changes reversed

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,19 @@ const express = require("express");
 const auth = require("./routes/authRoute");
 const userRoute = require("./routes/userRoutes");
 const {mongoose} = require("mongoose");
-const path = require("path");
+// const path = require("path");
 const app = express()
 app.use(express.json())
 
 app.use("/api/auth",auth);
 app.use("/api/user",userRoute)
 
-//serving the frontend
+// //serving the frontend
 
-app.use(express.static(path.join(__dirname,'./frontend/build')))
-app.get("*",function(req,res){
-  res.sendFile(path.join(__dirname,"./frontend/public/index.html"))
-})
+// app.use(express.static(path.join(__dirname,'./frontend/build')))
+// app.get("*",function(req,res){
+//   res.sendFile(path.join(__dirname,"./frontend/public/index.html"))
+// })
 
 mongoose.connect(process.env.MONGO_URI)
 .then(
